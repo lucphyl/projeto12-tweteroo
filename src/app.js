b92c2fc450916a80109102e749d5f49e298ef011
lucphyl <107371429+lucphyl@users.noreply.github.com>
2023-04-09 04:39:27 -0300

    post e get "/tweets"
     implementado post e get para a pagica "/tweets", testado puxando os ultimos 10 itens do array.

--- src/app.js
+++ src/app.js
@@ -22,11 +22,28 @@ App.post("/sign-up", (request,retorno)=> {
     const { username, avatar } = request.body;
 
     usuarios.push({username, avatar});
-
     retorno.send("ok, tudo funfando");
 })
 
+App.post("/tweets", (request,retorno)=> {
+    const postagem = request.body;
+
+    const cadastrado = usuarios.find(item => item.username === postagem.username);
+    if (!cadastrado) {
+        return(retorno.send("UNAUTHORIZED, vacilou ai chapa"));
+    }
+    tweets.push(postagem);
+    retorno.send("ok,tudo funfando");
+})
+
+App.get("/tweets", (request,retorno)=> {
+
+    const postados = tweets.map(tweet => {
+        const icone = usuarios.find(item => item.username === tweet.username);
+        return {...tweet,avatar: icone.avatar };
+    })
+    retorno.send(postados.slice(-10));
+})
 
 const PORTA = 5000
-
 App.listen( PORTA, ()=> console.log({PORTA}));
