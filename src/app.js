a789a01fba94be07d8e9b364b60b0098f109cb37
lucphyl <107371429+lucphyl@users.noreply.github.com>
2023-04-10 16:26:10 -0300

    array vazio ( sem dados iniciais )

--- src/app.js
+++ src/app.js
@@ -6,16 +6,10 @@ App.use(express.json());
 App.use(cors());
 
 const usuarios = [
-    {
-        username: 'jiraya',
-        avatar: 'https://64.media.tumblr.com/264050e3c779435129463ad9848ea1e4/tumblr_inline_p6m5hslrDc1rqtyie_1280.jpg'
-    }
+
 ];
 const tweets = [
-    {
-        username:'jiraya',
-        tweet: 'Eu amo uma mulher, mas não vou obrigá-la a me amar, vou cercá-la com meu amor enquanto... rezo por sua felicidade.'
-    }
+
 ];
 
 App.post("/sign-up", (request,retorno)=> {
